8f66a230e4687335ff0da8c266245afb70fe3872
alex garag <alex.garag@bk.ru>
2024-06-06 12:34:01 +0300

    new Project->GitHub

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import static WorkingNumbers.WorkingNumbers.*;
 
 public class Main {
     public static void main(String[] args) {
+
         System.out.println(flipNumber(-4256));
         System.out.println(getMaxDigitNumber(-980));
         System.out.println(getMinDigitNumber(-1239));
